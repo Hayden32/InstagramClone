ef579ae7b8657e1d4e0302aebabdc776ad80cab2
Hayden Hastings <haydenhastings32@gmail.com>
2019-10-28 14:05:52 -0600

    Added code for the profile image in the Feed Cell.

--- Instagram/Instagram/Views/FeedableViewCell.swift
+++ Instagram/Instagram/Views/FeedableViewCell.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class FeedableViewCell: UITableViewCell {
 
+    // MARK: - Outlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameTitleButton: UIButton!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likesCountLabel: UILabel!
+    @IBOutlet weak var postCommentLabel: UILabel!
+    @IBOutlet weak var commentCountButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+        profileImageView.layer.masksToBounds = true
+        selectionStyle = UITableViewCell.SelectionStyle.none
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
