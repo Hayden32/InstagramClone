5bb95c76fcb3b1ed1ce6f1a96a9e07c78b193cbd
Hayden Hastings <haydenhastings32@gmail.com>
2019-10-29 16:08:06 -0600

    Created the search view controller file and added a image view for the collection view

---
+++ Instagram/Instagram/Views/ExploreCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExploreCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Hayden Hastings on 10/29/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var exploreImageView: UIImageView!
+}
